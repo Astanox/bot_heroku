50ad07c5b347bcd0c7e0ab151229b7864ff4735c
Astanox <astaastanox@gmail.com>
2021-08-02 19:01:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ Client.db = require('./db.json')
 Client.on('guildMemberAdd', (member) => {
     // Lorsqu'un utilisateur rejoin.
     let welcomeChannel = Client.channels.cache.get('851198220494831626');
-    welcomeChannel.send(`${member} vient de rejoindre le serveur souhaitez lui la Bienvenue,\n Nous somme désormais ${member.guild.memberCount} 🎉 ! \n\n Pense a prendre tes <#842297550991523840> !`);
+    welcomeChannel.send(`➺ ${member} vient de rejoindre le serveur souhaitez lui la Bienvenue,\n\n・Nous somme désormais ${member.guild.memberCount} 🎉 ! \n\n・Pense a prendre tes <#842297550991523840> !`);
 
     member.roles.add('861938290529402942');
 });
