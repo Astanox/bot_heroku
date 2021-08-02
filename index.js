0deda8862a942bfa54304c58d5e14f00f503658c
Astanox <astaastanox@gmail.com>
2021-08-02 18:12:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ dotenv.config()
 
 Client.db = require('./db.json')
 
-Client.on('guildMemberAdd', (member, Client) => {
+Client.on('guildMemberAdd', (member) => {
     // Lorsqu'un utilisateur rejoin.
-    let welcomeChannel = Client.channels.cache.get('866762820396908557');
-    welcomeChannel.send(`${member} vient de rejoindre le serveur souhaitez lui la Bienvenue, Nous somme désormais ${member.guild.memberCount} 🎉 \n\n Pense a prendre tes <#851198220494831626> !`)
+    let welcomeChannel = client.channels.cache.get('866762820396908557');
+    welcomeChannel.send(`${member} vient de rejoindre le serveur souhaitez lui la Bienvenue, Nous somme désormais ${member.guild.memberCount} 🎉 \n\n Pense a prendre tes <#851198220494831626> !`);
+
+    member.roles.add('841982248840724480');
 });
 
 
